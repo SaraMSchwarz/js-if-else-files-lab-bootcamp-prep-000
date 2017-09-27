7837fbc3155f2e940752e0b258a94ae51b667548
Sara Schwarz <saramschwarz@gmail.com>
2017-09-27 14:43:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ if (name === "Susan") {
   console.log("Expected the name to be Susan")
   }
 
+if (height === "70") {
+  console.log("This is Susan's height")
+} else {
+  console.log("This is Joe's height")
+}
+
 // Don't worry about this
 module.exports = { name, height
 }
