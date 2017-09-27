c72415bf73f28a81230b9685ff5c10e17fab99f5
Sara Schwarz <saramschwarz@gmail.com>
2017-09-27 14:53:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ if (name === "Susan") {
   console.log("Expected the name to be Susan")
   }
 
-if (height === "70") {
+if (height = 70) {
   console.log("This is Susan's height")
 } else {
   console.log("This is Joe's height")
